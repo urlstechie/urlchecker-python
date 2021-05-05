276ba3699b5433362f700f5fc5b45b73ad5f26e1
Andrew Erickson <aerickson@mozilla.com>
2021-05-05 15:36:27 -0700

    show path, add comment

--- urlchecker/client/check_ci.py
+++ urlchecker/client/check_ci.py
@@ -40,8 +40,10 @@ def main(args, extra):
 
     files = args.files
 
+    # TODO: remove this leading spacing...
+    # - pre-commit strips leading spacing, so the first item is off
     # Alert user about settings
-    # print("              final path: %s" % path)  # TODO: show this as save path? only UrlChecker:save_report uses (but save includes path?)
+    print("              final path: %s" % path)
     print("                   files: %s" % files)
     print("               print all: %s" % (not args.no_print))
     print("           urls excluded: %s" % exclude_urls)
